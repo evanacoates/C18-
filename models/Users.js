fbf0b5d3b8a00b79e75207c359fac78e62c4eea5
Evan <evanacoates@gmail.com>
2023-04-24 10:54:40 -0400

    Users Schema

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,18 @@
 const { Schema, model } = require('mongoose');
 
-const UsersSchema = new Schema();
+const UsersSchema = new Schema(  {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    thoughts:{}
+    });
 
 module.exports = Users;
